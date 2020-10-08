f845952b398b4e09d865c6a06b81f38b9baae72c
Peter Bauer <p.bauer@htl-leonding.ac.at>
2020-10-08 22:53:06 +0200

    Fixed naming of win jfx

--- run-app-win.cmd
+++ run-app-win.cmd
@@ -1,1 +1,1 @@
-java -jar --module-path="javafx-sdk-15/lib" --add-modules="javafx.base,javafx.controls,javafx.fxml" nbmgui.jar
+java -jar --module-path="javafx-sdk-15-win/lib" --add-modules="javafx.base,javafx.controls,javafx.fxml" nbmgui.jar
